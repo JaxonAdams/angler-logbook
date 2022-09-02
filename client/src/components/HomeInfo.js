088d787c3416861776d4c87afe61046df04d9e74
Jaxon Adams <jadamsresume1@gmail.com>
2022-09-02 13:44:42 -0600

    home info section

---
+++ client/src/components/HomeInfo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { GeoAltFill, ThermometerHalf, Inboxes, Rulers } from 'react-bootstrap-icons';
+
+const HomeInfo = () => {
+    return (
+        <div className='home-info'>
+            <h2 className='home-info-title'>We'll help you track...</h2>
+            <div className='info-icon-container'>
+                <div className='info-icon'>
+                    <div className='icon-background'>
+                        <GeoAltFill className='icon' />
+                    </div>
+                    <p className='info-txt'>Location</p>
+                </div>
+                <div className='info-icon'>
+                    <div className='icon-background'>
+                        <ThermometerHalf className='icon' />
+                    </div>
+                    <p className='info-txt'>Temperature</p>
+                </div>
+                <div className='info-icon'>
+                    <div className='icon-background'>
+                        <Inboxes className='icon' />
+                    </div>
+                    <p className='info-txt'>Tackle Used</p>
+                </div>
+                <div className='info-icon'>
+                    <div className='icon-background'>
+                        <Rulers className='icon' />
+                    </div>
+                    <p className='info-txt'>Fish Size</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomeInfo;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import HomeInfo from '../components/HomeInfo';
 
 const Home = () => {
     return (
         <div className='home'>
             <Header />
             <Hero />
+            <HomeInfo />
         </div>
     );
 };
