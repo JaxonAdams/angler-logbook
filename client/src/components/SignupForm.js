e0d538e7b6a9fb0e6f6118b6c704c99a0aa61866
Jaxon Adams <jadamsresume1@gmail.com>
2022-09-05 16:26:40 -0600

    signup image photo

---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignupForm = () => {
+    return (
+        <form className='signup-form'>Hello World</form>
+    );
+};
+
+export default SignupForm;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import SignupForm from '../components/SignupForm';
 
 const Signup = ({ menuOpen, setMenuOpen }) => {
     return (
         <div className='signup'>
             <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-            Hello World
+            <div className='signup-container'>
+                <SignupForm />
+                <div className='signup-img' />
+            </div>
         </div>
     );
 };
