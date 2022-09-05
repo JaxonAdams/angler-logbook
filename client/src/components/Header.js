79fc64f15d4e9f434a72b04abe61f5b0601538b8
Jaxon Adams <jadamsresume1@gmail.com>
2022-09-04 18:36:48 -0600

    link to signup set up in header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [activePage, setActivePage] = useState('');
+
+    useEffect(() => {
+        const location = window.location.pathname.split('/')[1];
+        setActivePage(location);
+    }, []);
+
     return (
         <header className='header'>
             <Link to='/'>
                 <h1 className='header-title'>Angler's <span>Logbook</span></h1>
             </Link>
+            <div className={`header-nav ${activePage === 'signup' && 'current-page'}`}>
+                <Link to='/signup'>Sign Up</Link>
+            </div>
         </header>
     );
 };
