990d74a10165754d0a83acb9ab5e6bdca0c19603
Jaxon Adams <jadamsresume1@gmail.com>
2022-09-07 16:20:32 -0600

    login form title and subtitle

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,10 @@ import React from 'react';
 const LoginForm = () => {
     return (
         <form className='login-form'>
-            Hello World
+            <div>
+                <h1 className='form-title'>Log In to Your Account</h1>
+                <p className='form-subtitle'>Get back to logging what you catch!</p>
+            </div>
         </form>
     );
 };
