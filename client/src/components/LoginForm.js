c593cf4419c8f41440ce432e86d43ae86fd5563a
Jaxon Adams <jadamsresume1@gmail.com>
2022-09-07 16:24:42 -0600

    finalized login form --functionality needed

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
     return (
         <form className='login-form'>
             <div>
-                <h1 className='form-title'>Log In to Your Account</h1>
+                <h1 className='form-title'>Log Into Your Account</h1>
                 <p className='form-subtitle'>Get back to logging what you catch!</p>
             </div>
+            <input className='signup-form-input' type='email' placeholder='Email' />
+            <input className='signup-form-input' type='password' placeholder='Password' />
+            <button className='submit-btn' type='submit'>Log In</button>
+            <Link className='txt-link form-link' to='/signup'>Sign Up Instead</Link>
         </form>
     );
 };
