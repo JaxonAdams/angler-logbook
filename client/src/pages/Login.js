de437a90345f4957b6339a357afe3dd666702188
Jaxon Adams <jadamsresume1@gmail.com>
2022-09-07 16:35:03 -0600

    page title change on login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Header from '../components/Header';
 import LoginForm from '../components/LoginForm';
 
 const Login = ({ menuOpen, setMenuOpen }) => {
+    useEffect(() => {
+        document.title = 'Log In'
+    }, []);
+    
     return (
         <div className='login'>
             <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
