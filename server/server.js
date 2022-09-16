13635dfd747397a35acb8aef78502e675a7697c0
Jaxon Adams <jadamsresume1@gmail.com>
2022-09-16 10:24:32 -0600

    production build WIP

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/api', routes);
 
 // if in production, provide static assets in build folder
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '/client/build')));
 };
 
 // start server
