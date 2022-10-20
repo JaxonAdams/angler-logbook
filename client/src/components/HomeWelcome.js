03382add6e300e37bfe06426191aab832c8ba9a2
Jaxon Adams <jadamsresume1@gmail.com>
2022-10-20 16:41:09 -0600

    updated homepage

--- client/src/components/HomeWelcome.js
+++ client/src/components/HomeWelcome.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomeWelcome = () => {
     return (
@@ -6,6 +7,7 @@ const HomeWelcome = () => {
             <div className='welcome-container'>
                 <h3 className='welcome-title'>Welcome to the Community!</h3>
                 <p className='welcome-txt'>Browse other anglers' logs to keep up with your friends or see what lures are working nearby!</p>
+                <Link to='/view-all' className='btn-link no-hover'>Recently Caught</Link>
             </div>
             <div className='welcome-img-container'>
                 <div className='community-photo' />
