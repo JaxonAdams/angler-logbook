3cce75027e8d2912590d50061704b24a8bc4abf8
Jaxon Adams <jadamsresume1@gmail.com>
2022-10-24 18:12:30 -0600

    set up route for uploading image to s3

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 // import routes from other files in this folder
 const userRoutes = require('./user-routes');
 const logRoutes = require('./log-routes');
+const imageRoutes = require('./image-routes');
 
 // use routes with the specified url
 router.use('/api/users', userRoutes);
 router.use('/api/logs', logRoutes);
+router.use('/api/images');
 
 module.exports = router;
